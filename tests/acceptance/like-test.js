3c28bde523733b99ab9da4f59039a967c58822cb
Anil <anilmaurya8dec@gmail.com>
2016-01-11 16:12:04 +0530

    Add acceptance test

---
+++ tests/acceptance/like-test.js
@@ -0,0 +1,65 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../../tests/helpers/start-app';
+
+module('Acceptance | like', {
+  beforeEach: function() {
+    this.application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+test('add heartAnimation class on like event', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    click('.heart');
+    andThen(function(){
+      assert.equal(find('.heart.heartAnimation').length, 1);
+    });
+  });
+});
+
+test('remove heartAnimation class on unlike event', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    click('.heart');
+    andThen(function(){
+      assert.equal(find('.heart.heartAnimation').length, 1);
+      click('.heart.heartAnimation');
+      andThen(function(){
+        assert.equal(find('.heart.heartAnimation').length, 0);
+      });
+    });
+  });
+});
+
+test('increase like count on like event', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    click('.heart');
+    andThen(function(){
+      assert.equal(find('.heartCount').text().trim(), "201");
+    });
+  });
+});
+
+test('decrease like count on unlike event', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    click('.heart');
+    andThen(function(){
+      assert.equal(find('.heartCount').text().trim(), "201");
+      click('.heart.heartAnimation');
+      andThen(function(){
+        assert.equal(find('.heartCount').text().trim(), "200");
+      });
+    });
+  });
+});
